bca9ba5d94944311e289c733148d5eb849a5d39e
sebastianaro <sebastianaro@gmail.com>
2019-10-10 23:28:33 -0400

    added list with thumbnails
    added list with thumbnails

---
+++ includes/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<title>Sebas' blog</title>
+<link rel="stylesheet" href="includes/main.css">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<!--Fonts from google fonts -->
+<link href="https://fonts.googleapis.com/css?family=Baloo&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,900&display=swap" rel="stylesheet">
+
