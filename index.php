45d58832b8ebda337c468f40936676687efe243b
Alex Reitz <alexander.w.reitz@gmail.com>
2019-10-12 20:08:46 -0400

    added search and sort function

--- index.php
+++ index.php
@@ -42,45 +42,55 @@ function getRecipesFromDatabase() {
 
 
 <main>
-
+<div id="recipe-list" >
+			<input class="search" placeholder="Search" />
+  			<button class="sort" data-sort="recipe-title">Sort</button>
 	<ul class="list img-list">
-			<?php
+		<?php
 
-$recDetails = getRecipesFromDatabase();
-foreach ($recDetails as $recDetails) {
-	echo '
-<li class="list-item">
-<a href="recipes.php?title=' . $recDetails["title"] . '" class="inner">
-<div class="li-img">
-<img src="' . $recDetails["photo"] . '" Picture of ' . $recDetails["title"] . '" />
+			$recDetails = getRecipesFromDatabase();
+			foreach ($recDetails as $recDetails) {
+				echo '
+			<li class="list-item">
+				<a href="recipes.php?title=' . $recDetails["title"] . '" class="inner">
+					<span class="li-img">
+						<img src="' . $recDetails["photo"] . '" Picture of ' . $recDetails["title"] . '" />
+					</span>
+					<span class="li-text">
+						<h4 class="li-head recipe-title">' . $recDetails["title"] . '</h4>
+						<p class="li-sub">' . $recDetails["instructions"] . '</p>
+					</span>
+				</a>
+			</li>';
+			}
+			/*
+			$recTitles = getRecipeTitlesFromDatabase();
+			foreach ($recTitles as $recTitle) {
+			echo '
+			<li class="list-item">
+			<a href="recipes.php?title=' . $recTitle . '" class="inner">
+			<div class="li-text">
+			<h4 class="li-head">' . $recTitle . '</h4>
+			</div>
+			</a>
+			</li>';
+			}
+			*/
+		?>
+	</ul>
 </div>
 
-<div class="li-text">
-<h4 class="li-head">' . $recDetails["title"] . '</h4>
-<p class="li-sub">' . $recDetails["instructions"] . '</p>
-</div>
-</a>
-</li>';
-}
-/*
-$recTitles = getRecipeTitlesFromDatabase();
-foreach ($recTitles as $recTitle) {
-echo '
-<li class="list-item">
-<a href="recipes.php?title=' . $recTitle . '" class="inner">
-<div class="li-text">
-<h4 class="li-head">' . $recTitle . '</h4>
-</div>
-</a>
-</li>';
-}
- */
-?>
-		</ul>
 
 	</main>
 <footer>
-
+	<script>
+		$(document).ready(function(){
+			var options = {
+				valueNames: [ 'recipe-title' ]
+			};
+			var recList = new List('recipe-list', options);
+		})
+	</script>
 </footer>
 </body>
 </html>
