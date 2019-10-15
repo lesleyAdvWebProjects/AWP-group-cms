f6dcaae9c5ab3f4229bb7a410982d05b5094acc4
lesleygitholly <56518820+lesleygitholly@users.noreply.github.com>
2019-10-15 15:15:38 -0500

    added shopping list to menu

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@ ini_set('display_errors', 1); //show errors for debugging
 <div id="header">
     <div class="site-title"><a class="site-title-link"  href="index.php">Dev Foodies</a></div>
 		<div id="nav">
-		 <a href="index.php">Home</a> | <a href="post-recipe.php">Add a Recipe</a><br/>
+		  <a href="index.php">Home</a> | 
+                  <a href="post-recipe.php">Add a Recipe</a><br/> |
+	          <a href="shopping-list.php">View Shopping List</a> 	
 		</div>
 </div>
