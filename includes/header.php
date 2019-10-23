27acd51d5ebcf79c330213934854c1f6d7fe8c81
Andie Campos <andiecampos@Andies-MBP.attlocal.net>
2019-10-22 19:14:32 -0500

    debugged jquery conflicts with list.js

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Baloo&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,900&display=swap" rel="stylesheet">
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+   
 
 </head>
 <?php
--- recipes.php
+++ recipes.php
@@ -2,7 +2,7 @@
 include 'includes/header.php';
 
 ?>
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <body>
 <?php
