ea0f08dcb7f55a527a3a2e093c0f37e94450ef19
lesleygitholly <56518820+lesleygitholly@users.noreply.github.com>
2019-10-26 12:57:13 -0500

    adding ingredients to submit form

--- post-recipe.php
+++ post-recipe.php
@@ -9,10 +9,13 @@
 		<input type="text" name="recAuthor" />	<br /><br />
 		<label for="recDate">Date (YYYY/MM/DD)</label><br />
 		<input type="date" name="recDate" />	<br /><br />
-        <label for="recPic">Photo URL</label><br />
+                <label for="recPic">Photo URL</label><br />
 		<input type="text" name="recPic" />	<br /><br />
 		<label for="recSteps">Instructions</label><br />
 		<textarea name="recSteps"></textarea><br /><br />
+                <label for="recIngredients">Ingredients (Separate with Comma)</label>
+                <textarea name="recIngredients"></textarea>
+        
         
 		<input type="submit" value="Submit Recipe">
 		</form>
